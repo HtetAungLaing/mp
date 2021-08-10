1e4e6462cba01eb29d39f91faf8d225dcd0bc58b
HtetAungLaing <myueway98@gmail.com>
2021-08-10 21:35:41 +0630

    final

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::get("/post/{id}", "PostController@show")->name("post.show");
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-// Auth::routes(['register' => false]);
+// Auth::routes();
+Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/feedback', 'FeedbackController@store')->name('feedback.store');
 
