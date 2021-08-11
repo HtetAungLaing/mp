4fead75d96b48e2223d49a942bbb0e42d5e71743
HtetAungLaing <myueway98@gmail.com>
2021-08-11 19:10:57 +0630

    final

--- resources/views/components/css.blade.php
+++ resources/views/components/css.blade.php
@@ -1,4 +1,22 @@
 {{ $slot }}
+<!-- Primary Meta Tags -->
+<title>Mydarksidecollection</title>
+<meta name="title" content="Mydarksidecollection">
+<meta name="description" content="🔥 All of porn you want to watch is here 🔥">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://metatags.io/">
+<meta property="og:title" content="Mydarksidecollection">
+<meta property="og:description" content="🔥 All of porn you want to watch is here 🔥">
+<meta property="og:image" content="{{ asset('storage/poster/mdsc.jpg') }}">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://metatags.io/">
+<meta property="twitter:title" content="Mydarksidecollection">
+<meta property="twitter:description" content="🔥 All of porn you want to watch is here 🔥">
+<meta property="twitter:image" content="{{ asset('storage/poster/mdsc.jpg') }}">
 {{-- <link rel="icon" href="{{ asset('storage/photo/logo.jpg') }}" sizes="20x20" type="image/jpg"> --}}
 <link rel="stylesheet" href="{{ asset('css/mine/my.css') }}">
 <link rel="stylesheet" href="{{ asset('vendor/feather-icons-web/feather.css') }}">
